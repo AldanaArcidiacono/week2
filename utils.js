fa1868580dfe5249aaf8c1d1bc375f969d0fd238
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-10 16:22:09 +0200

    Updated utils

--- utils.js
+++ utils.js
@@ -2,8 +2,16 @@
 // function foo() {}
 // // Asignar
 // const lar = function () {}
-// Asignacion arrow
 
+// Otra maneta de export
+// export function bar() {
+//     const x = 22 * 2;
+//     if (x > 12) {
+//         console.log(x);
+//     }
+// }
+
+// Asignacion arrow
 export const foo = () => {
     const x = 22 * 2;
     if (x > 12) {

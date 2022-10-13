3395a183e31241e286978ef4ad6ccae98c8223e4
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-13 11:51:27 +0200

    Adding error control

--- testing/sample.js
+++ testing/sample.js
@@ -2,9 +2,24 @@ export function add(a, b) {
     if (a === 0 || b === 0) {
         return 'Error';
     }
-    return a + b;
+    return +a + Number(b);
 }
 
 export function subtraction(a, b) {
     return a - b;
+}
+
+export function division(a, b) {
+    const zeroError = new Error('No se puede dividir por cero');
+    const paramError = new Error('Parametro no validos');
+    if (a === 0 || b === 0) {
+        throw zeroError;
+    }
+    if (a === null || b === null) {
+        throw paramError;
+    }
+    if (isNaN(a) || isNaN(b)) {
+        throw paramError;
+    }
+    return a / b;
 }
--- testing/sample.test.js
+++ testing/sample.test.js
@@ -1,4 +1,4 @@
-import { add, subtraction } from './sample.js';
+import { add, division, subtraction } from './sample.js';
 
 describe('Testing add', () => {
     test('If a = 2 and b = 2 add should be', () => {
@@ -11,22 +11,43 @@ describe('Testing add', () => {
         let b = 2;
         expect(add(a, b)).toBeLessThan(5);
     });
-    test('If a = 0, return should be "Error', () => {
-        let a = 0;
-        let b = 2;
-        expect(add(a, b)).toBe('Error');
-    });
-    test('If b = 0, return should be "Error', () => {
-        let a = 2;
-        let b = 0;
-        expect(add(a, b)).toBe('Error');
-    });
 });
 
 describe('Testing subtraction', () => {
     test('If a = 8 and b = 5 the result should be', () => {
         let a = 8;
         let b = 5;
-        expect(subtraction(a, b)).toBe(0);
+        expect(subtraction(a, b)).toBe(3);
+    });
+});
+
+describe('Testing subtraction', () => {
+    test('If a = 4 and b = 2 add should be', () => {
+        let a = 4;
+        let b = 2;
+        expect(division(a, b)).toBe(2);
+    });
+    test('If a = 0, return should be "Error', () => {
+        let a = 0;
+        let b = 2;
+        expect(() => {
+            division(a, b);
+        }).toThrow();
+    });
+    test('If b = 0, return should be "Error"', () => {
+        let a = 2;
+        let b = 0;
+        expect(() => {
+            division(a, b);
+        }).toThrow();
+    });
+    test('If a = null, return should be "Error"', () => {
+        let a = null;
+        let b = 5;
+        try {
+            division(a, b);
+        } catch (e) {
+            expect(e.message).toBe('Parametro no validos');
+        }
     });
 });

8ae84331bb0ff178c7c7460cc2930d706892c25a
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-17 11:10:54 +0200

    Challenge week 2

---
+++ gol/gol.js
@@ -0,0 +1,27 @@
+export const calcNeighborSum = (i = 0, j = 0, data = []) => {
+    const positions = [
+        [i - 1, j - 1],
+        [i - 1, j],
+        [i - 1, j + 1],
+        [i, j - 1],
+        [i, j + 1],
+        [i + 1, j - 1],
+        [i + 1, j],
+        [i + 1, j + 1],
+    ];
+
+    const filterPositions = positions.filter(
+        (item) =>
+            item[0] >= 0 &&
+            item[1] >= 0 &&
+            item[0] < data.length &&
+            item[1] < data.length
+    );
+
+    let result = 0;
+    filterPositions.forEach((item) => {
+        result += data[item[0]][item[1]];
+    });
+
+    return result;
+};
---
+++ gol/gol.test.js
@@ -0,0 +1,38 @@
+import { calcNeighborSum } from './gol.js';
+
+const mock = [
+    [1, 0, 1],
+    [0, 1, 1],
+    [1, 1, 0],
+];
+
+const testCases = [
+    { i: 0, j: 0, r: 1 },
+    { i: 0, j: 1, r: 4 },
+    { i: 0, j: 2, r: 2 },
+
+    { i: 1, j: 0, r: 4 },
+    { i: 1, j: 1, r: 5 },
+    { i: 1, j: 2, r: 3 },
+
+    { i: 2, j: 0, r: 2 },
+    { i: 2, j: 1, r: 3 },
+    { i: 2, j: 2, r: 3 },
+];
+
+describe('Given calcNeighborSum ', () => {
+    testCases.forEach((item) => {
+        test(`When i = ${item.i}, j = ${item.j}, then result should be ${item.r}`, () => {
+            // Arrange
+            const i = item.i;
+            const j = item.j;
+            const expectedResult = item.r;
+
+            // Act
+            const result = calcNeighborSum(i, j, mock);
+
+            // Assert
+            expect(result).toBe(expectedResult);
+        });
+    });
+});
